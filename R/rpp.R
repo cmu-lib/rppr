e717881a878c9cef45d028ab66a8323d080862be
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-10-15 11:58:34 -0400

    no longer plotting arcs

--- R/rpp.R
+++ R/rpp.R
@@ -55,8 +55,7 @@ showme <- function(g) {
   ggraph(g, layout = "fr") +
     geom_edge_link(aes(color = target, alpha = original),
                   start_cap = circle(3, 'mm'),
-                  end_cap = circle(3, 'mm'),
-                  curvature = 0.4) +
+                  end_cap = circle(3, 'mm')) +
     geom_node_label(aes(label = pid)) +
     scale_edge_colour_manual(values = c("TRUE" = "red", "FALSE" = "gray"), na.value = "gray", guide = FALSE) +
     scale_edge_alpha_manual(values = c("TRUE" = 1, "FALSE" = 0.2, guide = FALSE)) +
