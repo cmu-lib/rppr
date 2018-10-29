cd5d2e7942f944eda5d4edd164bd58312125d31d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-10-29 15:03:28 -0400

    test for adjacent node ids

--- tests/testthat/test-rpp.R
+++ tests/testthat/test-rpp.R
@@ -7,3 +7,12 @@ test_that("subnetwork of targeted edges", {
   # Expect all nodes have at least one connection
   expect_false(any(degree(subset_network, mode = "all") == 0))
 })
+
+test_that("target adjacent node ids", {
+  tids <- target_adjacent_node_oids(rppr_graph)
+  expect_true(all(map_lgl(
+    # edge sets to test
+    incident_edges(rppr_graph, tids, "total"),
+    # Every edge set should include at least one target edge
+    function(x) any(edge_attr(rppr_graph, name = ".target", index = x)))))
+})
